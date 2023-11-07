05311183fecba051688b725955fff572204b8608
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-11-07 22:33:06 +0530

    feat: add learnings

--- oops.php
+++ oops.php
@@ -1,3 +1,40 @@
 <?php
-echo "he";
+class User
+{
+    public $name;
+    private $age;
+
+    public function __construct($age, $name)
+    {
+        $this->age = $age;
+        $this->name = $name;
+    }
+    private function hello()
+    {
+        return $this->name;
+    }
+    function printAge()
+    {
+        echo "The age is " . $this->age . " and name is " . $this->hello();
+    }
+}
+
+class Student extends User
+{
+    public $email;
+    public $marks;
+    public function __construct($age, $name, $email, $marks)
+    {
+        parent::__construct($age, $name);
+        $this->email = $email;
+        $this->marks = $marks;
+    }
+    public function get_email()
+    {
+        return $this->email;
+    }
+}
+$user = new User(10, "Ramesh");
+$student = new Student(10, "Ramesh", "ramesh@ramesh.com", 10);
+echo $user->printAge();
 ?>
