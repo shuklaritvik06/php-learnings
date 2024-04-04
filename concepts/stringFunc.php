0c04197c29b8f462df6f782bd843636d1d1785bc
Ritvik Shukla <ritvikshukla261@gmail.com>
2024-04-04 16:45:45 +0530

    structure creation

--- concepts/stringFunc.php
+++ concepts/stringFunc.php
@@ -3,7 +3,7 @@ $string = "Hello World!";
 echo str_pad($string, 23, "He", STR_PAD_LEFT);
 echo str_repeat($string, 2);
 echo str_shuffle($string);
-echo str_word_count($string);
+echo str_word_count($string); 
 echo str_split($string, 2);
 echo strlen($string);
 echo strpos($string, "e");
---
+++ practice.php
@@ -0,0 +1,2 @@
+<?php
+echo file_put_contents("./file.txt",[1,"hehe"]);
